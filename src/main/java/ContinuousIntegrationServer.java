2d46dcea1fa6a928ad166043186913d3b0f3fb79
oscar <osclind@kth.se>
2021-02-08 11:15:17 +0100

    reformat/#14
    Reformatted print to test a URLdecoder, and JSON decoding
    
    Another test-commit

--- src/main/java/ContinuousIntegrationServer.java
+++ src/main/java/ContinuousIntegrationServer.java
@@ -4,10 +4,12 @@ import javax.servlet.ServletException;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.handler.AbstractHandler;
+import org.eclipse.jetty.util.ajax.JSON;
 
 /** 
  Skeleton of a ContinuousIntegrationServer which acts as webhook
@@ -26,33 +28,47 @@ public class ContinuousIntegrationServer extends AbstractHandler {
         baseRequest.setHandled(true);
 
         System.out.println(target);
+        System.out.println(request.getMethod());
 
         // here you do all the continuous integration tasks
         // for example
         // 1st clone your repository
         // 2nd compile the code
-        switch (target) {
-            case "/" -> build(baseRequest, request, response);
-            case "/favicon.ico" -> response.getWriter().write("NO");
-            default -> {
-                String[] targetParams;
-                targetParams = target.split("/");
-                // splitting on "/" gives an empty string as first element since first character of target is a "/"
-                switch (targetParams[1]) {
-                    case "build" -> search(targetParams[2], baseRequest, request, response);
-                    case "allBuilds" -> history(baseRequest, request, response);
-                    default -> response.getWriter().write(
-                            "<html>" +
-                                "<head>" +
-                                    "<title>404 - not found</title>" +
-                                "</head>" +
-                                "<body style=\"vertical-align: center\">" +
-                                    "<div style=\"display: inline-block; text-align: center;\">" +
-                                        "<img src=\"https://cdn.searchenginejournal.com/wp-content/uploads/2020/08/killer-404-page-coschedule-5f3d58c828b04.png\">" +
-                                    "</div>" +
-                                "</body>" +
-                            "</html>");
-                }
+        if (target.equals("/")) {
+            if (request.getMethod().equals("POST"))
+                build(baseRequest, request, response);
+            else {
+                // possibly put build history here
+                response.getWriter().write(
+                    "<html>" +
+                        "<head>" +
+                            "<title>404 - not found</title>" +
+                        "</head>" +
+                        "<body style=\"vertical-align: center\">" +
+                            "<div style=\"display: inline-block; text-align: center;\">" +
+                                "<img src=\"https://cdn.searchenginejournal.com/wp-content/uploads/2020/08/killer-404-page-coschedule-5f3d58c828b04.png\">" +
+                            "</div>" +
+                        "</body>" +
+                    "</html>");
+            }
+        } else {
+            String[] targetParams;
+            targetParams = target.split("/");
+            // splitting on "/" gives an empty string as first element since first character of target is a "/"
+            switch (targetParams[1]) {
+                case "build" -> search(targetParams[2], baseRequest, request, response);
+                case "allBuilds" -> history(baseRequest, request, response);
+                default -> response.getWriter().write(
+                    "<html>" +
+                        "<head>" +
+                            "<title>404 - not found</title>" +
+                        "</head>" +
+                        "<body style=\"vertical-align: center\">" +
+                            "<div style=\"display: inline-block; text-align: center;\">" +
+                                "<img src=\"https://cdn.searchenginejournal.com/wp-content/uploads/2020/08/killer-404-page-coschedule-5f3d58c828b04.png\">" +
+                            "</div>" +
+                        "</body>" +
+                    "</html>");
             }
         }
         response.getWriter().flush();
@@ -69,11 +85,10 @@ public class ContinuousIntegrationServer extends AbstractHandler {
      */
     private void build(Request baseRequest, HttpServletRequest request, HttpServletResponse response) throws IOException {
         System.out.println("Something got pushed");
-        BufferedReader requestReader = request.getReader();
-        String line;
-        while ((line = requestReader.readLine()) != null) {
-            System.out.println(line);
-        }
+        String payload = java.net.URLDecoder.decode(request.getParameter("payload"), StandardCharsets.UTF_8);;
+        System.out.println(payload);
+        Object payloadMap = JSON.parse(payload);
+        System.out.println(JSON.toString(payloadMap));
         /*
          * send request to GitHub to mark commit as pending
          * clone repo
